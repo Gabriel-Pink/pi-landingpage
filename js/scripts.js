26e7c717e8f5076fb71fc22a41f10bb7d2e17bf2
Gabriel Rosa <aws@gabriel-rosa.com>
2025-03-14 12:17:48 -0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+document.querySelectorAll('.button').forEach(button => {
+    button.addEventListener('click', function() {
+        alert('Obrigado por seu interesse! Entraremos em contato em breve.');
+    });
+});
